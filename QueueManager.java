7502add74799b32085eb0ccdf6f2f5aa43896d03
Rafael de Lima <heretheysleep@rafaelftdelima.localdomain>
2022-11-06 16:41:34 -0300

    Queue manager class completely implemented

--- QueueManager.java
+++ QueueManager.java
@@ -1,85 +1,157 @@
 import java.util.Scanner;
 
-public class QueueManager extends Queue {
+public class QueueManager {
     private Scanner scanner;
+    private Queue mainQueue;
+    private Queue firstAuxiliaryQueue;
+    private Queue secondAuxiliaryQueue;
 
-    public QueueManager(int length) {
-        super(length);
+    public QueueManager() {
         scanner = new Scanner(System.in);
+        mainQueue = null;
+        firstAuxiliaryQueue = null;
+        secondAuxiliaryQueue = null;
     }
 
     public void run() {
         int option = 0;
-        int firstQueueLength = 0;
-        int secondQueueLength = 0;
 
-        System.out.println("Merge Queues Application\nFirst, type the queues's length");
+        System.out.println("Welcome to QMA\nQueues Merge Application\n\nFirst, insert the main queue length:");
+        mainQueue = new Queue(readQueueLength(0));
 
-        firstQueueLength = readQueueLength(1);
-        secondQueueLength = readQueueLength(2);
+        System.out.println("\nSecond, insert the auxiliary queues's length");
+        firstAuxiliaryQueue = new Queue(readQueueLength(1));
+        secondAuxiliaryQueue = new Queue(readQueueLength(2));
+
+        while (option != 5) {
+            option = showOptions();
+        }
+
+        System.out.println("\n\nSee you soon.");
+    }
+
+    private int readQueueLength(int numberOfQueue) {
+        int length = 0;
+
+        if (numberOfQueue != 0) {
+            System.out.println("Queue " + numberOfQueue + ":");
+        }
 
         while (true) {
-            System.out.println("\n(1) Add process\n(2) Execute process\n(3) Turn off\n\nOption:");
+            try {
+                length = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException ignored) {
+                System.out.println("Error: invalid input. Try again.\n\nQueue " + numberOfQueue + ":");
 
+                continue;
+            }
+
+            if (length <= 0) {
+                System.out.println("Error: invalid length. Try again.\n\nQueue " + numberOfQueue + ":");
+            } else {
+                break;
+            }
+        }
+
+        return length;
+    }
+
+    private int showOptions() {
+        int option = 0;
+
+        System.out.println("\n\nChoise an action\n\n(1) Fill queue 1\n" +
+            "(2) Fill queue 2\n(3) Merge queues\n(4) Clear main queue\n(5) Stop program\n\nOption:");
+
+        while (true) {
             try {
                 option = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException ignored) {
-                System.out.println("\nError: invalid input\nTry again");
+                System.out.println("Error: invalid input. Try again.\n\nOption:");
 
                 continue;
             }
 
             switch (option) {
                 case 1:
-                case 2:
+                    fillAuxiliaryQueue(1);
                     break;
-
+                case 2:
+                    fillAuxiliaryQueue(2);
+                    break;
                 case 3:
-                    System.out.println("\nSee you soon.");
-                    return;
+                    mainQueue.mergeQueues(firstAuxiliaryQueue, secondAuxiliaryQueue);
+                    break;
+                case 4:
+                    printMainQueueElements();
+                    break;
+                case 5:
+                    return option;
 
                 default:
-                    System.out.println("\nError: invalid option\nTry again");
-                    break;
+                    System.out.println("Error: invalid option. Try again.\n\nOption:");
+                    continue;
             }
+
+            return option;
         }
     }
 
-    private int readQueueLength(int numberOfQueue) {
-        int input = 0;
+    private void fillAuxiliaryQueue(int numberOfQueue) {
+        Queue queue = numberOfQueue == 1 ? firstAuxiliaryQueue : secondAuxiliaryQueue;
+        String input = null;
+        int element = 0;
 
-        System.out.println("\nQueue " + numberOfQueue + ":");
+        if (queue.isFull()) {
+            System.out.println("\nOops! the auxiliary queue is full.");
 
-        while (true) {
-            try {
-                input = Integer.parseInt(scanner.nextLine());
-            } catch (NumberFormatException ignored) {
-                System.out.println("Error: invalid input\n\nTry again:");
+            return;
+        }
 
-                continue;
+        while (!queue.isFull()) {
+            System.out.println("\nType an number or 'B' to back to main menu:");
+            input = scanner.nextLine().toUpperCase();
+
+            if (input.equals("B")) {
+                break;
             }
 
-            if (input <= 0) {
-                System.out.println("Error: invalid length\n\nTry again:");
-            } else {
+            while (true) {
+                try {
+                    element = Integer.parseInt(input);
+                } catch (NumberFormatException ignored) {
+                    System.out.println("Error: invalid input. Try again.");
+    
+                    break;
+                }
+
+                queue.addElement(element);
+
                 break;
             }
         }
 
-        return input;
+        if (queue.isFull()) {
+            System.out.println("\nALERT: the queue is full.");
+        }
     }
-    
-    private void printMainQueueElements() {
-        String output = "";
 
-        for (int currentIndex = 0; !isEmpty(); currentIndex++) {
+    private void printMainQueueElements() {
+        String output = "\n\nRESULT:\n\n";
+
+        if (mainQueue.isEmpty()) {
+            System.out.println("\nOops! The main queue is empty.");
+
+            return;
+        }
+
+        for (int currentIndex = 0; !mainQueue.isEmpty(); currentIndex++) {
             if (currentIndex == 0) {
                 output += "[ ";
             }
 
-            output += removeElement();
+            output += mainQueue.removeElement();
 
-            if (!isEmpty()) {
+            if (!mainQueue.isEmpty()) {
                 output += ", ";
             } else {
                 output += " ]";
